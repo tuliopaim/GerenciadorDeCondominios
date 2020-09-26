172fab53d446cc976cd96c5d3b47791a63c3b3a6
tuliopaim <tutpaim@gmail.com>
2020-09-26 19:06:49 -0300

    Adiciona jQuery Mask Plugin e estrutura da tela de Registro

---
+++ GerenciadorDeCondominios.Web/ViewModels/RegistroViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeCondominios.Web.ViewModels
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Tamanho máximo de 40 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Telefone { get; set; }
+        public string Foto { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Tamanho máximo de 40 caracteres")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Tamanho máximo de 40 caracteres")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Tamanho máximo de 40 caracteres")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirme sua senha")]
+        [Compare("Senha", ErrorMessage = "As senhas não conferem")]
+        public string SenhaConfirmada { get; set; }
+
+    }
+}
