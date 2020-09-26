f987dc48a9f635f04bd512a33e1b697a5728fdbf
tuliopaim <tutpaim@gmail.com>
2020-09-26 19:47:17 -0300

    Adiciona reposito generico com metodos crud

---
+++ GerenciadorDeCondominio.Infrastructure/Interfaces/IRepositorioGenerico.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeCondominios.Infrastructure.Interfaces
+{
+    public interface IRepositorioGenerico<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> ObterTodos();
+
+        Task<TEntity> PegarPorId(int id);
+        Task<TEntity> PegarPorId(string id);
+
+        Task Inserir(TEntity entity);
+        Task Atualizar(TEntity entity);
+        Task Excluir(TEntity entity);
+        Task Excluir(int id);
+        Task Excluir(string id);
+
+    }
+}
---
+++ GerenciadorDeCondominio.Infrastructure/Repositorios/RepositorioGenerico.cs
@@ -0,0 +1,130 @@
+﻿using GerenciadorDeCondominios.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeCondominios.Infrastructure.Repositorios
+{
+    public class RepositorioGenerico<TEntity> : IRepositorioGenerico<TEntity> where TEntity : class
+    {
+        private readonly Contexto _contexto;
+
+        public RepositorioGenerico(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task Atualizar(TEntity entity)
+        {
+            try
+            {
+                _contexto.Set<TEntity>().Update(entity);
+                await _contexto.SaveChangesAsync();
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Excluir(TEntity entity)
+        {
+            try
+            {
+                _contexto.Set<TEntity>().Remove(entity);
+                await _contexto.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Excluir(int id)
+        {
+            try
+            {
+                var entity = await PegarPorId(id);
+                _contexto.Set<TEntity>().Remove(entity);
+                await _contexto.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Excluir(string id)
+        {
+            try
+            {
+                var entity = await PegarPorId(id);
+                _contexto.Set<TEntity>().Remove(entity);
+                await _contexto.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Inserir(TEntity entity)
+        {
+            try
+            {
+                await _contexto.AddAsync(entity);
+                await _contexto.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<IEnumerable<TEntity>> ObterTodos()
+        {
+            try
+            {
+                return await _contexto.Set<TEntity>().ToListAsync();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<TEntity> PegarPorId(int id)
+        {
+            try
+            {
+                return await _contexto.Set<TEntity>().FindAsync(id);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<TEntity> PegarPorId(string id)
+        {
+            try
+            {
+                return await _contexto.Set<TEntity>().FindAsync(id);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
