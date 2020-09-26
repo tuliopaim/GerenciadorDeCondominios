78f94a8effd9340e53177e9bab1ff19cb36f599c
tuliopaim <tutpaim@gmail.com>
2020-09-26 19:48:20 -0300

    Adiciona metodo de Registro no controller

--- GerenciadorDeCondominios.Web/Controllers/UsuariosController.cs
+++ GerenciadorDeCondominios.Web/Controllers/UsuariosController.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using GerenciadorDeCondominios.Domain.Entidades;
+using GerenciadorDeCondominios.Domain.Enumeradores;
+using GerenciadorDeCondominios.Infrastructure.Interfaces;
+using GerenciadorDeCondominios.Web.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GerenciadorDeCondominios.Web.Controllers
 {
     public class UsuariosController : Controller
     {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public UsuariosController(IUsuarioRepositorio usuarioRepositorio,
+            IWebHostEnvironment webHostEnvironment)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +34,82 @@ namespace GerenciadorDeCondominios.Web.Controllers
         public IActionResult Registro()
         {
             return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Registro(RegistroViewModel model, IFormFile foto)
+        {
+            if (ModelState.IsValid)
+            {
+                if(foto != null)
+                {
+                    string dirPastaImagem = Path.Combine(_webHostEnvironment.WebRootPath, "imagens");
+                    string nomeFoto = Guid.NewGuid().ToString();
+
+                    using var fileStream = new FileStream(Path.Combine(dirPastaImagem, nomeFoto), FileMode.Create);
+
+                    await foto.CopyToAsync(fileStream);
+
+                    model.Foto = "~/imagens/" + nomeFoto;
+
+                }
+
+                var usuario = new Usuario();
+                IdentityResult usuarioCriado;
+
+                if((await _usuarioRepositorio.VerificarSeExisteRegistro()) == 0)
+                {
+                    usuario.UserName = model.Nome;
+                    usuario.CPF = model.CPF;
+                    usuario.Email = model.Email;
+                    usuario.PhoneNumber = model.Telefone;
+                    usuario.Foto = model.Foto;
+                    usuario.PrimeiroAcesso = false;
+                    usuario.Status = StatusConta.Aprovado;
+
+                    usuarioCriado = await _usuarioRepositorio.CriarUsuario(usuario, model.Senha);
+
+                    if (usuarioCriado.Succeeded)
+                    {
+                        await _usuarioRepositorio.IncluirUsuarioEmFuncao(usuario, "Administrador");
+                        await _usuarioRepositorio.LogarUsuario(usuario, false);
+
+                        return RedirectToAction("Index", "Usuarios");
+                    }
+                }
+
+                usuario.UserName = model.Nome;
+                usuario.CPF = model.CPF;
+                usuario.Email = model.Email;
+                usuario.PhoneNumber = model.Telefone;
+                usuario.Foto = model.Foto;
+                usuario.PrimeiroAcesso = true;
+                usuario.Status = StatusConta.Analisando;
+
+                usuarioCriado = await _usuarioRepositorio.CriarUsuario(usuario, model.Senha);
+
+                if (usuarioCriado.Succeeded)
+                {
+                    return View("Analise", usuario.UserName);
+                }
+                else
+                {
+                    foreach(var erro in usuarioCriado.Errors)
+                    {
+                        ModelState.AddModelError("", erro.Description);
+                    }
+
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Analise(string nome)
+        {
+            return View(nome);
         }
     }
 }
